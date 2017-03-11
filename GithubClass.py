65155a3a91c3a9b3b8c5b0ae9c633b88d73736ab
jakobpederson <jakob.pederson+github@gmail.com>
2017-03-11 14:25:24 -0600

    Feature: multiprocessing and refactoring

--- GithubClass.py
+++ GithubClass.py
@@ -1,6 +1,30 @@
 #!python
-
+import csv
+import re
 from github import Github
+
+
+def get_repo_requirements_for_multiprocessing(token, repo):
+    branches = ["production", "master"]
+    h = GithubClass(token)
+    new_list = []
+    temp_list = []
+    for branch in branches:
+        files = h.try_get_branch_files(repo, branch)
+        temp_list = h.create_list_of_repo_requirements(repo, branch, "requirements", files)
+        h.split_out_versions(temp_list)
+        new_list.extend(temp_list)
+    return new_list
+
+
+def write_to_csv_file(list_of_lists):
+    with open("output.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(list_of_lists)
+
+
+def get_names_of(list_of_things):
+    return list(thing.name for thing in list_of_things)
 
 
 class GithubClass:
@@ -13,7 +37,8 @@ class GithubClass:
         return self.client
 
     def get_names_of_repos(self):
-        return list(set([x.name for x in self.client.get_user().get_repos()]))
+        repos = self.client.get_user().get_repos()
+        return list(set(get_names_of(repos)))
 
     def create_repo(self, repo_name):
         return self.client.get_user().create_repo(name=repo_name, auto_init=True)
@@ -22,62 +47,63 @@ class GithubClass:
         return self.client.get_user().get_repos()
 
     def get_repo(self, repo_name):
-        repo_list = list(self.get_repo_gen(repo_name))
-        return repo_list[0]
-
-    def get_repo_gen(self, repo_name):
         for repo in self.get_repos():
             if repo.name == repo_name:
-                yield repo
+                return repo
 
     def delete_repo(self, repo_name):
-        repo = self.get_repo(repo_name)
-        repo.delete()
+        self.get_repo(repo_name).delete()
 
     def get_names_of_branches(self, repo_name):
         if repo_name in self.get_names_of_repos():
             repo = self.get_repo(repo_name)
-            return list(set(list(x.name for x in repo.get_branches())))
-        else:
-            pass
+            branches = repo.get_branches()
+            return list(set(get_names_of(branches)))
 
     def create_branch(self, repo, branch_name):
         sha = repo.get_git_ref('heads/master').object.sha
-        branch = repo.create_git_ref('refs/heads/{}'.format(branch_name), sha)
-        return branch
+        return repo.create_git_ref('refs/heads/{}'.format(branch_name), sha)
 
     def get_branch(self, repo, branch_name):
         for branch in repo.get_branches():
             if branch.name == branch_name:
                 return branch
 
-    def get_protected_branch(self, repo, branch_name):
-        return repo.get_protected_branch(branch_name)
-
     def protect_branch(self, repo, branch_name):
         repo.protect_branch(branch_name, True, "everyone", ["test"])
+
+    def get_protected_branch(self, repo, branch_name):
+        return repo.get_protected_branch(branch_name)
 
     def create_file(self, repo, path, message, content, branch):
         return repo.create_file(path=path, message=message, content=content, branch=branch)
 
+    def try_get_branch_files(self, repo, branch_name):
+        try:
+            return self.get_branch_files(repo, "requirements", branch_name)
+        except Exception:
+            pass
+
     def get_branch_files(self, repo, path, branch_name):
         return repo.get_dir_contents(path, branch_name)
 
-    def convert_github_files(self, files):
+    def decode_github_files(self, files):
         data_in_files = []
-        for github_file in files:
-            data_in_files.extend(github_file.decoded_content.decode("utf-8").split('\n'))
-        return data_in_files
+        if files:
+            for github_file in files:
+                data_in_files.extend(github_file.decoded_content.decode("utf-8").split('\n'))
+        return list(value for value in data_in_files if value and value[0] != "-" and value[0] != "#")
 
     def get_repos_with_branch(self, branch_name):
         repos_with_branch = []
+        repos_with_branch.extend(list(self.get_repos_with_branch_gen(branch_name)))
+        return repos_with_branch
+
+    def get_repos_with_branch_gen(self, branch_name):
         for repo in self.get_repos():
-            try:
-                if branch_name in [x.name for x in repo.get_branches()]:
-                    repos_with_branch.append(repo.name)
-            except:
-                pass
-        return list(set(repos_with_branch))
+            branches = repo.get_branches()
+            if branch_name in get_names_of(branches):
+                yield repo
 
     def get_file_names(self, files):
         names_of_files = []
@@ -86,23 +112,11 @@ class GithubClass:
         return names_of_files
 
     def create_list_of_repo_requirements(self, repo, branch, path, files):
-        new_list = []
-        for item in self.convert_github_files(files):
-            new_list.append([repo.name, branch,  path, item])
-        return new_list
+        return list([repo.name, branch, item] for item in self.decode_github_files(files))
 
     def split_out_versions(self, list_of_lists):
-        import re
         for single_list in list_of_lists:
-            new_list = re.sub(r'(?<!,)([=<>])', r',\1', single_list[3], count=1)
-            single_list.pop(3)
+            new_list = re.sub(r'(?<!,)([=<>])', r',\1', single_list[2], count=1)
+            single_list.pop(2)
             single_list.extend(new_list.split(","))
         return list_of_lists
-
-    def create_repo_files_dict(self, list_of_repo_names, branch_name):
-        repo_files = {}
-        for repo_name in list_of_repo_names:
-            repo_files[repo_name] = self.get_branch_files(self.get_repo(repo_name), "requirements", branch_name)
-        return repo_files
-
-
